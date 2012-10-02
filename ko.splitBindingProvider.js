34973cab4e5d59128b8b238f55f1ad2aee95b65a
Neal Erickson <minus.twelve@gmail.com>
2012-10-01 22:02:52 -0600

    Working now in Hello World case (fixed issue with instance vs prototype while leveraging original ko binding provider evaluator function)

--- ko.splitBindingProvider.js
+++ ko.splitBindingProvider.js
@@ -1,59 +1,87 @@
 (function ($) {
 
+	// Prevent multiple loading
 	if (ko.splitBindingProvider != undefined) return;
 
+	// Sanity check (should be obvious)
 	if (!ko) throw new Error('Knockout JS must be loaded');
 
-	ko.splitBindingProvider = function (defaultBindingProvider) {
-		this.splitBindingAttributePrefix = "data-bind-";
-		this.defaultBindingProvider = defaultBindingProvider ? defaultBindingProvider : ko.bindingProvider.instance;
+	// Custom binding provider ctor declaration
+	//
+	// A custom binding provider must provide two functions:
+	//   --> 'nodeHasBindings(node)'
+	//   --> 'getBindings(node, bindingContext)'
+	ko.splitBindingProvider = function () {
+		var sbp = this;
 
-		this.nodeHasBindings = function (node) {
-			switch (node.nodeType) {
-				case 1: // Element node
-					// Check if we have a standard KO "data-bind" attribute - use that if needed
-					if (this.defaultBindingProvider.nodeHasBindings(node)) return true;
-					// Otherwise look for data-bind-* attributes
-					return this.checkNodeForSplitDataBindAttributes(node.attributes);
-				case 8: // Comment node - handled the default way
-					return this.defaultBindingProvider.nodeHasBindings(node);
-				default: return false;
-			}
+		// Split attributes begin with this (e.g. 'data-bind-text')
+		sbp.splitBindingAttributePrefix = "data-bind-";
+		// Leverages the default instance of the ko binding provider
+		sbp.defaultBindingProvider = ko.bindingProvider.instance;
+
+		// Internal : determines if the attribute is a split binding.
+		// .lastIndexOf() is supposed to be moderately fast
+		sbp.attributeIsSplitBinding = function (attributeName) {
+			return attributeName.lastIndexOf(sbp.splitBindingAttributePrefix, 0) === 0;
 		};
 
 		// Iterate through attributes, searching for "data-bind-*" names
-		this.checkNodeForSplitDataBindAttributes = function (attributes) {
+		sbp.checkNodeForSplitDataBindAttributes = function (attributes) {
 			for (var i = 0; i < attributes.length; i++) {
-				if (this.attributeIsSplitBinding(attributes[i].nodeName)) return true;
+				if (sbp.attributeIsSplitBinding(attributes[i].nodeName)) return true;
 			}
 			return false; // no data-bind-* to be found
 		};
 
-		this.attributeIsSplitBinding = function (attributeName) {
-			//return attributeName.substring(0, this.customBindingAttributeName.length) === this.customBindingAttributeName;
-			return attributeName.lastIndexOf(this.splitBindingAttributePrefix, 0) === 0;
+		// Called by ko to find nodes that must be data bound
+		sbp.nodeHasBindingsInternal = function (node) {
+			switch (node.nodeType) {
+				case 1: // Element node
+					// Check if we have a standard KO "data-bind" attribute - use that first
+					if (sbp.defaultBindingProvider.nodeHasBindings(node)) return true;
+					// Otherwise look for data-bind-* attributes
+					return sbp.checkNodeForSplitDataBindAttributes(node.attributes);
+				case 8: // Comment node - handled the default way
+					return sbp.defaultBindingProvider.nodeHasBindings(node);
+				default: return false;
+			}
+		};
+
+		// Debugging : just print comparison
+		sbp.nodeHasBindings = function(node) {
+			var hasBindingsInternal = sbp.nodeHasBindingsInternal(node);
+			var hasBindings = sbp.defaultBindingProvider.nodeHasBindings(node);
+			console.log('hasBindings -->', 'ko', hasBindings, 'sbp', hasBindingsInternal);
+			//return hasBindings; // default for now
+			return sbp.nodeHasBindingsInternal(node);
 		};
 
 		// This fxn returns a bindings object representing that active bindings for a given node
-		this.getBindings = function (node, bindingContext) {
+		sbp.getBindings = function (node, bindingContext) {
+
+			var originalBindings = sbp.defaultBindingProvider.getBindings(node, bindingContext);
+
+			console.log('og:', originalBindings);
 
 			// Check if we should use "old-school" KO data-bind attribute mechanism
-			if (this.defaultBindingProvider.nodeHasBindings(node)) {
-				return this.defaultBindingProvider.getBindings(node, bindingContext);
+			if (sbp.defaultBindingProvider.nodeHasBindings(node)) {
+				//return sbp.defaultBindingProvider.getBindings(node, bindingContext);
+				return originalBindings;
 			}
 
 			// Get split data-bind attributes
 			var bindingsString = "";
 			for (var i = 0; i < node.attributes.length; i++) {
-				if (this.attributeIsSplitBinding(node.attributes[i].nodeName)) {
+				if (sbp.attributeIsSplitBinding(node.attributes[i].nodeName)) {
 					if (i > 0) bindingsString += ',';
 					bindingsString +=
-						node.attributes[i].nodeName.substring(this.splitBindingAttributePrefix.length) + ':' + node.attributes[i].nodeValue;
+						node.attributes[i].nodeName.substring(sbp.splitBindingAttributePrefix.length) + ':' + node.attributes[i].nodeValue;
 				}
 			}
 
+			// BIG PROBLEMO : Binding cache is undefined...
 			// If the bindings were legitimate, leverage the ko binding providers parse bindings string method
-			return bindingsString ? ko.bindingProvider.prototype.parseBindingsString(bindingsString, bindingContext) : null;
+			return bindingsString ? sbp.defaultBindingProvider.parseBindingsString(bindingsString, bindingContext) : null;
 		};
 	};
 })(jQuery);
